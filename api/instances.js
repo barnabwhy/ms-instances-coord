fa0e0feeacf478b01c5b0113ce28daf81de2d5a2
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-01-13 17:58:43 +0000

    Update instances.js

--- api/instances.js
+++ api/instances.js
@@ -6,6 +6,7 @@ module.exports = async function handler(req, res, instances) {
   try {
     if(!req.query.id) return res.status(400).send()
     let ip = req.socket.remoteAddress;
+    if(process.env.TRUST_PROXY) ip = req.headers['x-real-ip'];
     if(ip.startsWith("::ffff:")) ip = ip.slice(7);
     let selfFound = false;
     let instancesWithSelfs = await Promise.all(instances.map(inst => {
